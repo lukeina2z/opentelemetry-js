5737689f167fe3b863be650e2cbf2ddf3bc7ee67
Luke Zhang <lukezha@amazon.com>
2025-06-11 12:39:01 -0700

    add more resource attributes.

--- examples/basic-tracer-node/index.js
+++ examples/basic-tracer-node/index.js
@@ -1,74 +1,91 @@
 'use strict';
 
 const opentelemetry = require('@opentelemetry/api');
-const { resourceFromAttributes } = require('@opentelemetry/resources');
+const { detectResources, resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { AsyncLocalStorageContextManager } = require("@opentelemetry/context-async-hooks");
 const { CompositePropagator, W3CTraceContextPropagator, W3CBaggagePropagator } = require("@opentelemetry/core");
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { envDetector, processDetector } = require('@opentelemetry/resources');
+const {
+  envDetector,
+  processDetector,
+  hostDetector,
+} = require('@opentelemetry/resources');
 
-// Configure span processor to send spans to the exporter
-const exporter = new OTLPTraceExporter({
-  url: 'http://localhost:4318/v1/traces',
-});
+(async () => {
+  await main();
+})();
 
-/**
- * Initialize the OpenTelemetry APIs to use the BasicTracerProvider bindings.
- *
- * This registers the tracer provider with the OpenTelemetry API as the global
- * tracer provider. This means when you call API methods like
- * `opentelemetry.trace.getTracer`, they will use this tracer provider. If you
- * do not register a global tracer provider, instrumentation which calls these
- * methods will receive no-op implementations.
- */
-opentelemetry.trace.setGlobalTracerProvider(new BasicTracerProvider({
-  resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: 'basic-service',
-  }),
-  spanProcessors: [
-    new SimpleSpanProcessor(exporter),
-    new SimpleSpanProcessor(new ConsoleSpanExporter()),
-  ],
-  resourceDetectors: [envDetector, processDetector]
-}));
-opentelemetry.context.setGlobalContextManager(new AsyncLocalStorageContextManager());
-opentelemetry.propagation.setGlobalPropagator(new CompositePropagator({
-  propagators: [
-    new W3CTraceContextPropagator(),
-    new W3CBaggagePropagator()]
-}));
+async function main() {
+  // Configure span processor to send spans to the exporter
+  const exporter = new OTLPTraceExporter({
+    url: 'http://localhost:80/v1/traces',
+  });
 
-const tracer = opentelemetry.trace.getTracer('example-basic-tracer-node');
+  const defaultResource = await detectResources({
+    detectors: [envDetector, processDetector, hostDetector],
+  });
+  const customResource = resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: 'my-svc-foo',
+  });
 
-// Create a span. A span must be closed.
-const parentSpan = tracer.startSpan('main');
-for (let i = 0; i < 10; i += 1) {
-  doWork(parentSpan);
-}
-// Be sure to end the span.
-parentSpan.end();
+  const mergedResource = defaultResource.merge(customResource);
 
-// flush and close the connection.
-exporter.shutdown();
+  /**
+   * Initialize the OpenTelemetry APIs to use the BasicTracerProvider bindings.
+   *
+   * This registers the tracer provider with the OpenTelemetry API as the global
+   * tracer provider. This means when you call API methods like
+   * `opentelemetry.trace.getTracer`, they will use this tracer provider. If you
+   * do not register a global tracer provider, instrumentation which calls these
+   * methods will receive no-op implementations.
+   */
+  opentelemetry.trace.setGlobalTracerProvider(new BasicTracerProvider({
+    resource: mergedResource,
+    spanProcessors: [
+      new SimpleSpanProcessor(exporter),
+      new SimpleSpanProcessor(new ConsoleSpanExporter()),
+    ],
+    resourceDetectors: [envDetector, processDetector]
+  }));
+  opentelemetry.context.setGlobalContextManager(new AsyncLocalStorageContextManager());
+  opentelemetry.propagation.setGlobalPropagator(new CompositePropagator({
+    propagators: [
+      new W3CTraceContextPropagator(),
+      new W3CBaggagePropagator()]
+  }));
 
-function doWork(parent) {
-  // Start another span. In this example, the main method already started a
-  // span, so that'll be the parent span, and this will be a child span.
-  const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parent);
-  const span = tracer.startSpan('doWork', undefined, ctx);
+  const tracer = opentelemetry.trace.getTracer('example-basic-tracer-node');
 
-  // simulate some random work.
-  for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
-    // empty
+  // Create a span. A span must be closed.
+  const parentSpan = tracer.startSpan('main');
+  for (let i = 0; i < 10; i += 1) {
+    doWork(parentSpan);
   }
+  // Be sure to end the span.
+  parentSpan.end();
 
-  // Set attributes to the span.
-  span.setAttribute('key', 'value');
+  // flush and close the connection.
+  exporter.shutdown();
 
-  // Annotate our span to capture metadata about our operation
-  span.addEvent('invoking doWork');
+  function doWork(parent) {
+    // Start another span. In this example, the main method already started a
+    // span, so that'll be the parent span, and this will be a child span.
+    const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parent);
+    const span = tracer.startSpan('doWork', undefined, ctx);
 
-  span.end();
+    // simulate some random work.
+    for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+      // empty
+    }
+
+    // Set attributes to the span.
+    span.setAttribute('key', 'value');
+
+    // Annotate our span to capture metadata about our operation
+    span.addEvent('invoking doWork');
+
+    span.end();
+  }
 }
