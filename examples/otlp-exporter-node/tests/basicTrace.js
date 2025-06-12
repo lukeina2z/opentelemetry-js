91dab0dfd32790b581737fdaf9e2d6058a0bb216
Luke Zhang <lukezha@amazon.com>
2025-06-12 13:55:03 -0700

    refactor code.

--- examples/otlp-exporter-node/tests/basicTrace.js
+++ examples/otlp-exporter-node/tests/basicTrace.js
@@ -1,11 +1,30 @@
 'use strict';
 
+function doWork(parent, oTelApi, tracer) {
+    // Start another span. In this example, the main method already started a
+    // span, so that'll be the parent span, and this will be a child span.
+    const ctx = oTelApi.trace.setSpan(oTelApi.context.active(), parent);
+    const span = tracer.startSpan('doWork', undefined, ctx);
+
+    // simulate some random work.
+    for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+        // empty
+    }
+
+    // Set attributes to the span.
+    span.setAttribute('color', 'green');
+
+    // Annotate our span to capture metadata about our operation
+    span.addEvent('invoking doWork');
+    span.end();
+}
+
 module.exports = async function main(oTelApi, oTelTracerProvider) {
     const tracer = oTelApi.trace.getTracer('example-basic-tracer-node');
     // Create a span. A span must be closed.
     const parentSpan = tracer.startSpan('main');
     for (let i = 0; i < 10; i += 1) {
-        doWork(parentSpan);
+        doWork(parentSpan, oTelApi, tracer);
     }
     // Be sure to end the span.
     parentSpan.end();
@@ -15,23 +34,4 @@ module.exports = async function main(oTelApi, oTelTracerProvider) {
     // oTelApi.shutdown();
     // exporter.shutdown();
     console.log(`Tracer is down now.`);
-
-    function doWork(parent) {
-        // Start another span. In this example, the main method already started a
-        // span, so that'll be the parent span, and this will be a child span.
-        const ctx = oTelApi.trace.setSpan(oTelApi.context.active(), parent);
-        const span = tracer.startSpan('doWork', undefined, ctx);
-
-        // simulate some random work.
-        for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
-            // empty
-        }
-
-        // Set attributes to the span.
-        span.setAttribute('color', 'green');
-
-        // Annotate our span to capture metadata about our operation
-        span.addEvent('invoking doWork');
-        span.end();
-    }
 }
