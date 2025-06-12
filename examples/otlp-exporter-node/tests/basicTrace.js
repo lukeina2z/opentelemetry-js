06f1850f812dc18ee0cc6b93610777466b4dc688
Luke Zhang <lukezha@amazon.com>
2025-06-12 13:50:09 -0700

    refactor the test code.

--- examples/otlp-exporter-node/tests/basicTrace.js
+++ examples/otlp-exporter-node/tests/basicTrace.js
@@ -1,98 +1,7 @@
 'use strict';
 
-const opentelemetry = require('@opentelemetry/api');
-const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-const { CompositePropagator, W3CTraceContextPropagator, W3CBaggagePropagator } = require("@opentelemetry/core");
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { AsyncLocalStorageContextManager } = require("@opentelemetry/context-async-hooks");
-
-
-// Auto instrumentation
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { FsInstrumentation } = require('@opentelemetry/instrumentation-fs');
-
-// Resource Detector
-const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-const { detectResources, resourceFromAttributes } = require('@opentelemetry/resources');
-const {
-    awsBeanstalkDetector,
-    awsEc2Detector,
-    awsEcsDetector,
-    awsEksDetector,
-    awsLambdaDetector
-} = require('@opentelemetry/resource-detector-aws');
-
-const varFoo = awsBeanstalkDetector.detect();
-
-const {
-    envDetector,
-    processDetector,
-    hostDetector
-} = require('@opentelemetry/resources');
-
-// const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-// const provider = new NodeTracerProvider();
-// provider.register();
-
-module.exports = async function testFn() {
-    // Configure span processor to send spans to the exporter
-    const exporter = new OTLPTraceExporter({
-        url: 'http://localhost:80/v1/traces',
-    });
-
-    const defaultResource = await detectResources({
-        detectors: [envDetector, processDetector, hostDetector, awsBeanstalkDetector,
-            awsEc2Detector,
-            awsEcsDetector,
-            awsEksDetector,
-            awsLambdaDetector],
-    });
-
-    const customResource = resourceFromAttributes({
-        [ATTR_SERVICE_NAME]: 'xxyy-svc-foo2',
-    });
-
-    const mergedResource = defaultResource.merge(customResource);
-
-    /**
-     * Initialize the OpenTelemetry APIs to use the BasicTracerProvider bindings.
-     *
-     * This registers the tracer provider with the OpenTelemetry API as the global
-     * tracer provider. This means when you call API methods like
-     * `opentelemetry.trace.getTracer`, they will use this tracer provider. If you
-     * do not register a global tracer provider, instrumentation which calls these
-     * methods will receive no-op implementations.
-     */
-    const tracerProvider = new BasicTracerProvider({
-        resource: mergedResource,
-        spanProcessors: [
-            new SimpleSpanProcessor(exporter),
-            new SimpleSpanProcessor(new ConsoleSpanExporter()),
-        ],
-        resourceDetectors: [envDetector, processDetector]
-    });
-    opentelemetry.trace.setGlobalTracerProvider(tracerProvider);
-    opentelemetry.context.setGlobalContextManager(new AsyncLocalStorageContextManager());
-    opentelemetry.propagation.setGlobalPropagator(new CompositePropagator({
-        propagators: [
-            new W3CTraceContextPropagator(),
-            new W3CBaggagePropagator()]
-    }));
-
-    registerInstrumentations({
-        tracerProvider: tracerProvider,
-        instrumentations: [
-            new FsInstrumentation({
-            }),
-            new HttpInstrumentation({
-            })]
-        // instrumentations: getNodeAutoInstrumentations(),
-    });
-
-    const tracer = opentelemetry.trace.getTracer('example-basic-tracer-node');
-
+module.exports = async function main(oTelApi, oTelTracerProvider) {
+    const tracer = oTelApi.trace.getTracer('example-basic-tracer-node');
     // Create a span. A span must be closed.
     const parentSpan = tracer.startSpan('main');
     for (let i = 0; i < 10; i += 1) {
@@ -102,12 +11,15 @@ module.exports = async function testFn() {
     parentSpan.end();
 
     // flush and close the connection.
-    exporter.shutdown();
+    await oTelTracerProvider.shutdown();
+    // oTelApi.shutdown();
+    // exporter.shutdown();
+    console.log(`Tracer is down now.`);
 
     function doWork(parent) {
         // Start another span. In this example, the main method already started a
         // span, so that'll be the parent span, and this will be a child span.
-        const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parent);
+        const ctx = oTelApi.trace.setSpan(oTelApi.context.active(), parent);
         const span = tracer.startSpan('doWork', undefined, ctx);
 
         // simulate some random work.
@@ -116,17 +28,10 @@ module.exports = async function testFn() {
         }
 
         // Set attributes to the span.
-        span.setAttribute('key', 'value');
+        span.setAttribute('color', 'green');
 
         // Annotate our span to capture metadata about our operation
         span.addEvent('invoking doWork');
-
         span.end();
     }
 }
-
-// export default function mainFn() {
-//     (async () => {
-//         await testFn();
-//     })();
-// }
