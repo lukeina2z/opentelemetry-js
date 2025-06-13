c7cd36a649d8887e3f85700e1aefae0172c2d844
Luke Zhang <lukezha@amazon.com>
2025-06-13 16:45:10 -0700

    tried to enable logging.

--- examples/otlp-exporter-node/tests/basicTrace.js
+++ examples/otlp-exporter-node/tests/basicTrace.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { context, trace, ROOT_CONTEXT } = require('@opentelemetry/api');
+
 function doWork(parent, oTelApi, tracer) {
     // Start another span. In this example, the main method already started a
     // span, so that'll be the parent span, and this will be a child span.
@@ -16,6 +18,13 @@ function doWork(parent, oTelApi, tracer) {
 
     // Annotate our span to capture metadata about our operation
     span.addEvent('invoking doWork');
+
+    const spanContext = trace.setSpan(context.active(), span);
+    context.with(spanContext, () => {
+        openFooFromProfileSync();
+        console.log(`traceId: ${spanContext.traceId}`);
+    });
+
     span.end();
 }
 
@@ -23,7 +32,7 @@ module.exports = async function main(oTelApi, oTelTracerProvider) {
     const tracer = oTelApi.trace.getTracer('example-basic-tracer-node');
     // Create a span. A span must be closed.
     const parentSpan = tracer.startSpan('main');
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < 3; i += 1) {
         doWork(parentSpan, oTelApi, tracer);
     }
     // Be sure to end the span.
@@ -34,4 +43,38 @@ module.exports = async function main(oTelApi, oTelTracerProvider) {
     // oTelApi.shutdown();
     // exporter.shutdown();
     console.log(`Tracer is down now.`);
+}
+
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+function openFooFromProfileSync() {
+    const homeDir = os.homedir();
+    const filePath = path.join(homeDir, 'foo.txt');
+
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        console.log(`File contents:\n${data}`);
+    } catch (err) {
+        console.error(`Failed to open file: ${err.message}`);
+    }
+}
+
+
+function openFooFromProfileAsync() {
+    // Get the user's home directory (e.g., /Users/your-username)
+    const homeDir = os.homedir();
+    
+    // Construct full path to foo.txt
+    const filePath = path.join(homeDir, 'foo.txt');
+
+    // Read the file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Failed to open file: ${err.message}`);
+            return;
+        }
+        console.log(`File contents:\n${data}`);
+    });
 }
